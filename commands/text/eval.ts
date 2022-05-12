153e30640b06beb293b57a9ce72355d520a4fa83
Sreecharan <sr33ch4@yahoo.com>
2022-05-12 11:49:12 +0530

    update: eval.ts

--- commands/text/eval.ts
+++ commands/text/eval.ts
@@ -1,5 +1,6 @@
 
 import { TextCommand } from '../../classes';
+const { inspect } = require('util');
 import messageCreate from '../../events/messageCreate';
 
 var sudo = [892689836249591849,532914066558156800,688308495384313866,782110657142194176]
@@ -7,13 +8,14 @@ var sudo = [892689836249591849,532914066558156800,688308495384313866,78211065714
 export default {
     name: 'eval',
 
-    callback: async ({ client, message, user, channel, args }) => {
+    callback: async ({ client, message, user, channel }) => {
         if (sudo.includes(user.id) == false ) return; // only authorized people can access this command 
         
-        const code = args.join(" ");
+        const code = message.content.split(" ").slice(1).join(" ");
 
         try {
-          let output = await eval(code);
+          let output = inspect(await eval(code));
+            
           message.channel.send(output, { code:'ts'})
         } catch (error) {
           message.channel.send('`eval() output is too long to display :/`')
