2d36ba224eb148c8cb0d2742309dea7a9ed325ed
Sreecharan <sr33ch4@yahoo.com>
2022-05-12 14:28:12 +0530

    fix: eval.ts

--- commands/text/eval.ts
+++ commands/text/eval.ts
@@ -8,18 +8,19 @@ var sudo = ["892689836249591849","532914066558156800","688308495384313866","7821
 export default {
     name: 'eval',
 
-    callback: async ({ client, message, user, channel }) => {
+    callback: async ({ client, message, user, channel, args }) => {
         if (sudo.includes(user.id) == false ) return; // only authorized people can access this command 
         
-        const code = message.content.split(" ").slice(1).join(" ");
+        //const code = message.content.split(" ").slice(1).join(" ");
+        const code = args.join(" ")
 
         try {
           let output = inspect(await eval(code));
             
-          message.reply({content: output, code: "ts"});
+          message.reply("```ts\n" + output + "\n```");
         } catch (error) {
             console.log(error);
-            message.reply({content: error, code: "ts"});
+            message.reply("```ts\n" + error + "\n```");
         }
     },
 } as TextCommand;
