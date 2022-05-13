e99966bdbeb3cfb45701d07a5accf93fd89d3a34
ChampX5 <89368086+ChampX5@users.noreply.github.com>
2022-05-13 15:39:48 +0530

    feat: changed how the game functions

--- commands/slash/misc/button.ts
+++ commands/slash/misc/button.ts
@@ -1,48 +1,63 @@
-import { MessageActionRow, MessageButton, MessageEmbed } from 'discord.js';
+import {
+    Message,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed
+} from 'discord.js';
 import { Command } from '../../../classes';
 
 export default {
-	name: 'game',
-	category: 'MISC',
-	description: 'Jobless? Want to click a button? Here!',
+    name: 'game',
+    category: 'MISC',
+    description: 'Jobless? Want to click a button? Here!',
 
-	callback: async ({ interaction, member, client }) => {
-		const row = new MessageActionRow().addComponents([
-			new MessageButton()
-				.setLabel('Anger')
-				.setStyle('DANGER')
-				.setCustomId('game-btn1'),
+    callback: async ({ interaction, member }) => {
+        const row = new MessageActionRow().addComponents(
+            new MessageButton()
+                .setLabel('Click!')
+                .setCustomId('game-btn')
+                .setStyle('SUCCESS')
+        );
 
-			new MessageButton()
-				.setLabel('Happy')
-				.setStyle('SUCCESS')
-				.setCustomId('game-btn2'),
+        const msg = await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                    .setTitle('Click the buttons!')
+                    .setColor('GREY')
+                    .setAuthor({
+                        name: member.displayName,
+                        iconURL: member.displayAvatarURL({ dynamic: true })
+                    })
+            ],
+            components: [row],
+            fetchReply: true
+        });
 
-			new MessageButton()
-				.setLabel('Not Really Happy')
-				.setStyle('PRIMARY')
-				.setCustomId('game-btn3'),
+        const collector = (msg as Message).createMessageComponentCollector({
+            componentType: 'BUTTON',
+            time: 7000,
 
-			new MessageButton()
-				.setLabel('Sad')
-				.setStyle('SECONDARY')
-				.setCustomId('game-btn4')
-		]);
+            filter: (inter) => {
+                return !inter.user.bot;
+            }
+        });
 
-		await interaction.reply({
-			embeds: [
-				new MessageEmbed()
-					.setTitle('Click the buttons!')
-					.setDescription(
-						"Click the **RED** if you're angry!\nClick the **GREEN** if you're really happy!\nClick the **BLUE** if you're not really happy!\nClick the **GREY** if you're sad!"
-					)
-					.setColor('GREY')
-					.setAuthor({
-						name: member.displayName,
-						iconURL: member.displayAvatarURL({ dynamic: true })
-					})
-			],
-			components: [row]
+		collector.on('collect', (inter) => {
+			collector.resetTimer();
 		});
-	}
+
+        collector.on('end', async () => {
+            await (msg as Message).edit({
+                components: [
+                    new MessageActionRow().addComponents(
+                        new MessageButton()
+                            .setLabel('Click!')
+                            .setCustomId('game-btn')
+                            .setStyle('DANGER')
+                            .setDisabled(true)
+                    )
+                ]
+            });
+        });
+    }
 } as Command;
