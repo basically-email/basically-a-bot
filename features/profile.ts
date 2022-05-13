fb5ad035265c326cbfedba3d1bb94e4e511770a8
Sreecharan <sr33ch4@yahoo.com>
2022-05-13 19:01:43 +0530

    fix: timezone

--- features/profile.ts
+++ features/profile.ts
@@ -7,8 +7,11 @@ export default {
     callback: async ({ client }) => {
         setInterval(() => {
             const date = new Date();
+            var currentOffset = date.getTimezoneOffset();
+            var Offset = 330;   // IST offset GMT +5:30 
+            var IST = new Date(date.getTime() + (Offset + currentOffset)*60000); //new Date(); in IST
 
-            if (date.getHours() > 6 && date.getHours() < 17) {
+            if (IST.getHours() > 6 && IST.getHours() < 17) {
                 if (client.user!.avatar !== 'https://media.discordapp.net/attachments/960507288416837642/973577011111006288/unknown.png?width=1238&height=1238') {
                     client.user!.setAvatar('https://media.discordapp.net/attachments/960507288416837642/973577011111006288/unknown.png?width=1238&height=1238');
                 }
